955c051df6c3c0f5838c90a7947e5f8ffc46a7ac
Jan Sedivy <jan.sedivy8@gmail.com>
2015-11-16 23:40:06 +0100

    fix indentation and warnings

--- src/utlis/login.js
+++ src/utlis/login.js
@@ -4,24 +4,24 @@ let token = null;
 
 module.exports = {
 
-	requestAccesToken(auth) {
-		let acessUrl = `https://www.reddit.com/api/v1/access_token`;
-		let authBasic = config.app_key + ': ';
-		var postObj = {
-			method: 'post',
-    	headers: {
-      	'Authorization': `Basic ${Base64.btoa(authBasic)}`
-    	},
-    	body: `grant_type=authorization_code&code=${auth.code}&redirect_uri=nativeforreddit://login`
-		}
-		return fetch(acessUrl, postObj).then((res) => {
-			return res.json();
-		}).then((final) => {
-			token = final;
-		})
-	},
+  requestAccesToken(auth) {
+    let acessUrl = `https://www.reddit.com/api/v1/access_token`;
+    let authBasic = config.app_key + ': ';
+    var postObj = {
+      method: 'post',
+      headers: {
+        'Authorization': `Basic ${Base64.btoa(authBasic)}`
+      },
+      body: `grant_type=authorization_code&code=${auth.code}&redirect_uri=nativeforreddit://login`
+    };
+    return fetch(acessUrl, postObj).then((res) => {
+      return res.json();
+    }).then((final) => {
+      token = final;
+    });
+  },
 
-	getToken() {
-		return token;
-	}
-}
+  getToken() {
+    return token;
+  }
+};
